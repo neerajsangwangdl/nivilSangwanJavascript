b2537e70bc54be0844aea264f49a2869aa679906
neerajsangwangdl <115098051+neerajsangwangdl@users.noreply.github.com>
2024-03-12 12:45:44 +0000

    Stack, Heap and Data Types Summary

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // function firstFunction() {
 //     document.getElementById('Nivil').innerHTML = 'I popped up';
 // }
-
+// -------------------------  1.Variables  -----------------------------
 // var x, y, z;
 // x = 5;
 // y = 10;
 // z = x + y;
 // console.log(x, y + z, z);
-
+// ----------------------- 2.Data Types --------------------------
 // let CarName = 'lymbergymber'
 // document.gr = Number(score)
 // console.log(typeof valueInNumber)
@@ -22,7 +22,7 @@
 // let booleanisloggedin = Boolean(IsloggedIn)
 // console.log(booleanisloggedin);
 
-//                                      OPERATIONS
+// --------------------------- 3.OPERATIONS ----------------------------
 // console.log(Boolean(2-2))        idk how this came to my mind and was correct
 
 // console.log("1" + 2);
@@ -30,4 +30,38 @@
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-console.log(+true);
+// console.log(+true);
+
+// --------------------------- 4.Comparison -----------------------------
+
+// console.log(2 > 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+
+// console.log( "2" > 1)
+// console.log( "02" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+/* ------------- 5.Data Types Summary --------------
+1. Primitive Data types
+
+7 Types: String, numbers, booleans, null, undefind, symbol, BigInt
+
+2. Reference/Non-Primitive Data types
+
+3 Types: Arreys, Objects, Functions.
+
+*/
+/* -------------------- Stack, Heap ---------------------
+1. Stack: Primitive
+2. Heap: Non-Primitive
+*/
+
