54b4402b3a1e34c7940577901d6bf9d85e9dccca
neerajsangwangdl <115098051+neerajsangwangdl@users.noreply.github.com>
2024-03-12 03:56:48 +0000

    Datatypes conversion

---
+++ index.js
@@ -0,0 +1,24 @@
+// function firstFunction() {
+//     document.getElementById('Nivil').innerHTML = 'I popped up';
+// }
+
+// window.alert('ae bhai');
+// var x, y, z;
+// x = 5;
+// y = 10;
+// z = x + y;
+// console.log(x, y + z, z);
+
+// let CarName = 'lymbergymber'
+// document.gr = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let score = '40fgh';
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let IsloggedIn = ""
+let booleanisloggedin = Boolean(IsloggedIn)
+console.log(booleanisloggedin);
