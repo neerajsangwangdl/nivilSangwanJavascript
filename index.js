7d8d6886abdf29d43082284d7d881f10cbfbf02e
neerajsangwangdl <115098051+neerajsangwangdl@users.noreply.github.com>
2024-03-14 12:19:46 +0000

    Arrays

--- index.js
+++ index.js
@@ -144,6 +144,18 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(4));
 
-const newArr = myArr.join()
-console.log(myArr);
-console.log(typeof newArr);
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+//slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log("B", myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
